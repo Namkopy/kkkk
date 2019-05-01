4bde6234cf2db8b54a16585de2c084f8b75e68bb
Kopy <kopy6836@gmail.com>
2019-05-01 09:04:06 +0700

    hmhjfh

---
+++ LMS/LMS/frmBorrowDetail.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace LMS
+{
+    partial class frmBorrowDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dgvBorrwDetail = new System.Windows.Forms.DataGridView();
+            this.dateFrom = new System.Windows.Forms.DateTimePicker();
+            this.dateTo = new System.Windows.Forms.DateTimePicker();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBorrwDetail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(239)))), ((int)(((byte)(229)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Location = new System.Drawing.Point(3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1329, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Minion Pro", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Blue;
+            this.label7.Location = new System.Drawing.Point(640, 8);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(385, 84);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Borrow Detail";
+            // 
+            // dgvBorrwDetail
+            // 
+            this.dgvBorrwDetail.AllowDrop = true;
+            this.dgvBorrwDetail.AllowUserToAddRows = false;
+            this.dgvBorrwDetail.AllowUserToDeleteRows = false;
+            this.dgvBorrwDetail.AllowUserToOrderColumns = true;
+            this.dgvBorrwDetail.AllowUserToResizeColumns = false;
+            this.dgvBorrwDetail.AllowUserToResizeRows = false;
+            this.dgvBorrwDetail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvBorrwDetail.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SunkenVertical;
+            this.dgvBorrwDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBorrwDetail.Location = new System.Drawing.Point(52, 239);
+            this.dgvBorrwDetail.Name = "dgvBorrwDetail";
+            this.dgvBorrwDetail.ReadOnly = true;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Chantrea SR UI Low", 12F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Blue;
+            this.dgvBorrwDetail.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvBorrwDetail.RowTemplate.Height = 28;
+            this.dgvBorrwDetail.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvBorrwDetail.Size = new System.Drawing.Size(1217, 296);
+            this.dgvBorrwDetail.TabIndex = 3;
+            this.dgvBorrwDetail.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dgvBorrwDetail_RowPostPaint);
+            // 
+            // dateFrom
+            // 
+            this.dateFrom.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.dateFrom.CustomFormat = "dd/MM/yyyy";
+            this.dateFrom.Font = new System.Drawing.Font("Chantrea SR UI Low", 12F);
+            this.dateFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateFrom.Location = new System.Drawing.Point(253, 146);
+            this.dateFrom.Name = "dateFrom";
+            this.dateFrom.Size = new System.Drawing.Size(200, 39);
+            this.dateFrom.TabIndex = 4;
+            // 
+            // dateTo
+            // 
+            this.dateTo.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.dateTo.CustomFormat = "dd/MM/yyyy";
+            this.dateTo.Font = new System.Drawing.Font("Chantrea SR UI Low", 12F);
+            this.dateTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTo.Location = new System.Drawing.Point(567, 145);
+            this.dateTo.Name = "dateTo";
+            this.dateTo.Size = new System.Drawing.Size(200, 39);
+            this.dateTo.TabIndex = 5;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Chantrea SR UI Low", 12F);
+            this.btnSubmit.Location = new System.Drawing.Point(847, 143);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(123, 40);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.ForeColor = System.Drawing.Color.Coral;
+            this.label3.Location = new System.Drawing.Point(485, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 37);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "To";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(45, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 37);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Date From";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtTotal.Location = new System.Drawing.Point(1229, 560);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(39, 44);
+            this.txtTotal.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(978, 560);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 37);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Total Amout";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Chantrea SR UI Low", 12F);
+            this.btnRefresh.Location = new System.Drawing.Point(1071, 145);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(123, 40);
+            this.btnRefresh.TabIndex = 23;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // frmBorrowDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(1332, 625);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.dateTo);
+            this.Controls.Add(this.dateFrom);
+            this.Controls.Add(this.dgvBorrwDetail);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmBorrowDetail";
+            this.Text = "frmBorrowDetail";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBorrowDetail_FormClosing);
+            this.Load += new System.EventHandler(this.frmBorrowDetail_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBorrwDetail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dgvBorrwDetail;
+        private System.Windows.Forms.DateTimePicker dateFrom;
+        private System.Windows.Forms.DateTimePicker dateTo;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
---
+++ LMS/LMS/frmBorrowDetail.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LMS
+{
+    public partial class frmBorrowDetail : Form
+    {
+        public static int m;
+        public frmBorrowDetail()
+        {
+            InitializeComponent();
+            m = 1;
+        }
+       
+        private void frmBorrowDetail_Load(object sender, EventArgs e)
+        {
+            SQLDB.DB.SQL_Grid(dgvBorrwDetail  , "SELECT * FROM v_BorrowDetail");
+            this.MaximizeBox = false;
+            dgvBorrwDetail.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            double result = 0;
+            for (int i = 0; i < dgvBorrwDetail.Rows.Count; i++)
+                result += Convert.ToDouble(dgvBorrwDetail.Rows[i].Cells["Price"].Value);
+            txtTotal.Text = result.ToString() + "R";
+        }
+
+        private void dgvBorrwDetail_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            var grid = sender as DataGridView;
+            var rowIdx = (e.RowIndex + 1).ToString();
+            var centerFormat = new StringFormat()
+            {
+                //right alignment might actually make more sense for numbers
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            var headerBounds = new Rectangle(e.RowBounds.Left, e.RowBounds.Top, grid.RowHeadersWidth, e.RowBounds.Height);
+            e.Graphics.DrawString(rowIdx, this.Font, SystemBrushes.ControlText, headerBounds, centerFormat);
+        }
+
+        private void frmBorrowDetail_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            m = 0;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            SQLDB.DB.SQL_Grid(dgvBorrwDetail, "SELECT * FROM v_BorrowDetail where [BorrowDate] between '" + dateFrom.Value + "' and '" + dateTo.Value + "' ");
+            this.MaximizeBox = false;
+            dgvBorrwDetail.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            double result = 0;
+            for (int i = 0; i < dgvBorrwDetail.Rows.Count; i++)
+                result += Convert.ToDouble(dgvBorrwDetail.Rows[i].Cells["Price"].Value);
+            txtTotal.Text = result.ToString() + "R";
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            SQLDB.DB.SQL_Grid(dgvBorrwDetail, "SELECT * FROM v_BorrowDetail");
+            this.MaximizeBox = false;
+            dgvBorrwDetail.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            double result = 0;
+            for (int i = 0; i < dgvBorrwDetail.Rows.Count; i++)
+                result += Convert.ToDouble(dgvBorrwDetail.Rows[i].Cells["Price"].Value);
+            txtTotal.Text = result.ToString() + "R";
+        }
+
+       
+    }
+}
